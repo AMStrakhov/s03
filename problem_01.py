534446aa4a2cd8ed8f1226f18ffc152b50009f45
zisam295 <zisam295@gmail.com>
2022-09-18 18:01:23 +0300

    Initial Problem and Tests.

---
+++ problem_01.py
@@ -0,0 +1,25 @@
+"""
+Description:
+
+Write a script that calculates the greatest common divisor for integer inputs: a and b.
+Using of the `math` module is not allowed.
+
+Example:
+Input: 20
+8
+Output: 4
+
+Input: 9
+12
+Output: 3
+
+"""
+
+
+def main() -> None:
+    # Write your solution here:
+    ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_problem_01.py
@@ -0,0 +1,18 @@
+from typing import Callable, List, Tuple
+
+from problem_01 import main
+from .base_test_problem import BaseTestProblem
+
+
+class TestProblem01(BaseTestProblem):
+    _problem_func: Callable = main
+    cases: List[Tuple[List, str, str]] = [
+        (["12", "8"], "4", "12 & 8"),
+        (["20", "8"], "4", "20 & 8"),
+        (["0", "1"], "1", "0 & 1"),
+        (["12", "12"], "12", "12 & 12"),
+    ]
+    modules_to_patch = ["math.gcd", "problem_01.gcd"]
+
+    def test_problem(self) -> None:
+        self._test_problem()
