a6bde97b72d60faef3636bff46ac3816b75c7a03
AMStrakhov <a.strakhov@vk.team>
2022-09-18 19:00:47 +0300

    Problem #2: Formatting and Minor Fixes.

--- tests/test_problem_02.py
+++ tests/test_problem_02.py
@@ -4,7 +4,7 @@ from problem_02 import main
 from .base_test_problem import BaseTestProblem
 
 
-class TestProblem01(BaseTestProblem):
+class TestProblem02(BaseTestProblem):
     _problem_func: Callable = main
     cases: List[Tuple[List, str, str]] = [
         (["0"], "", "0"),
@@ -23,21 +23,14 @@ class TestProblem01(BaseTestProblem):
                 "0 1 2 3 4 5 6 7 8 9\n"
                 "0 1 2 3 4 5 6 7 8 9"
             ),
-            "10"
+            "10",
         ),
         (
             ["5"],
-            (
-                "0 1 2 3 4\n"
-                "0 1 2 3 4\n"
-                "0 1 2 3 4\n"
-                "0 1 2 3 4\n"
-                "0 1 2 3 4"
-            ),
-            "5"
-        )
+            "0 1 2 3 4\n0 1 2 3 4\n0 1 2 3 4\n0 1 2 3 4\n0 1 2 3 4",
+            "5",
+        ),
     ]
-    modules_to_patch = ["math.gcd", "problem_01.gcd"]
 
     def test_problem(self) -> None:
         self._test_problem()
