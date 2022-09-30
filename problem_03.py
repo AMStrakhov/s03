e3597e956519f998e35989b365b226b3767b8a85
a.strakhov <a.strakhov@corp.mail.ru>
2022-09-30 19:18:54 +0300

    Fix

--- problem_03.py
+++ problem_03.py
@@ -4,7 +4,7 @@ Description:
 Write a function that counts the number of `1` in a binary representation of a
 positive integer. If the input is not a positive integer, raise an exception.
 Use bitwise operations. Usage of strings is not allowed.
-5 -> 0b101 -> 3
+5 -> 0b101 -> 2
 
 Example:
 Input: 1
